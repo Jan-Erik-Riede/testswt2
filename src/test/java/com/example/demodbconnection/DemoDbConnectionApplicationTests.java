11a12bb7c1b9744a4daef540ee11d71596bebf38
Jan Erik <jan-erik.riede@stud.th-luebeck.de>
2021-04-24 18:05:11 +0200

    first commit

---
+++ src/test/java/com/example/demodbconnection/DemoDbConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodbconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDbConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
